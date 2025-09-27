6735500c101b7242a364e40bc06ff558261b6e9c
ebenezeradekanmi <ebenezeradekanmi2018@gmail.com>
2025-09-27 14:45:12 +0100

    Add perform_operation function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Perform basic arithmetic operations on two numbers.
     """
@@ -14,3 +14,4 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 / num2
     else:
         return "Error: Invalid operation"
+
