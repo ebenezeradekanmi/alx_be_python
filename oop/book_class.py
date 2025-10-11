c6276803c700d0a2938a19bda110b7c235a67169
ebenezeradekanmi <ebenezeradekanmi2018@gmail.com>
2025-10-11 14:17:04 +0100

    Implement Book class with magic methods (__init__, __del__, __str__, __repr__)

---
+++ oop/book_class.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self, title, author, year):
+        """Constructor that initializes a Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor that prints a message when a Book instance is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """Returns a user-friendly string representation of the Book."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Returns an official string representation of the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
